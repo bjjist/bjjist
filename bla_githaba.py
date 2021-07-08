06cd0ab6f13b7f8ea61b633edcb82c13df70279e
bjjist <tukanbro5@gmail.com>
2021-07-08 19:50:30 +0300

    Firs commit)))

---
+++ bla_githaba.py
@@ -0,0 +1,19 @@
+import hashlib
+
+
+hashlib.sha224
+obj = hashlib.sha224(b"hello world")
+xesh1 = obj.hexdigest()
+
+
+
+
+hashlib.sha384
+obj2 =haslib.sha384(b"hello world")
+xesh2 = obj2.hexdigest()
+
+
+
+hashlib.sha256
+obj3 = hashlib.sha256(b"hello world")
+xesh3 = obj3.hexdigest()
